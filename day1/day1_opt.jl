2f12f248ca49a1a4d34144cbe044fb712f96d4e9
Carl Wiede <guardsmancarl@gmail.com>
2023-12-01 10:04:18 +0100

    oneliner

---
+++ day1/day1_opt.jl
@@ -0,0 +1,9 @@
+include("../ReadString.jl")
+input = ReadString.read_input()
+
+numstrings = [("one", 1), ("two", 2), ("three", 3), ("four", 4),
+              ("five", 5), ("six", 6), ("seven", 7), ("eight", 8),
+              ("nine", 9), ("1", 1), ("2", 2), ("3", 3), ("4", 4),
+              ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)]
+
+println(mapreduce(x->parse(Int,"$(last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (findfirst(ns[1], x), ns[2]), numstrings))))))$(last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (findlast(ns[1], x), ns[2]), numstrings))))))"), +, input))
