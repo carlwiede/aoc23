e1970fde11fd6128ba239635d1d72362d597cacd
Carl Wiede <guardsmancarl@gmail.com>
2023-12-01 11:04:53 +0100

    Fixed error and increased readability

--- day1/day1_opt.jl
+++ day1/day1_opt.jl
@@ -6,4 +6,4 @@ numstrings = [("one", 1), ("two", 2), ("three", 3), ("four", 4),
               ("nine", 9), ("1", 1), ("2", 2), ("3", 3), ("4", 4),
               ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)]
 
-println(mapreduce(x->parse(Int,mapreduce(f->map(x->"$x", last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (f.(ns[1], x), ns[2]), numstrings)))))), *, [findfirst, findlast])), +, input))
+println(mapreduce(input_line -> parse(Int,mapreduce(f -> map(digitVal -> "$digitVal", last(reduce((x, y) -> f[2](x[1], y[1]) ? x : y, map(posTuple -> (first(posTuple[1]), posTuple[2]), filter(posInStr -> posInStr[1] !== nothing, map(ns -> (f[1](ns[1], input_line), ns[2]), numstrings)))))), *, [(findfirst, <), (findlast, >)])), +, input))
