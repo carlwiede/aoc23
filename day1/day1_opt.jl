f7a5451cd3ec94f77c2b41a182aedf477fb47a64
Carl Wiede <guardsmancarl@gmail.com>
2023-12-01 10:38:14 +0100

    Made oneliner shorter and therefore considerably more readable

--- day1/day1_opt.jl
+++ day1/day1_opt.jl
@@ -6,4 +6,4 @@ numstrings = [("one", 1), ("two", 2), ("three", 3), ("four", 4),
               ("nine", 9), ("1", 1), ("2", 2), ("3", 3), ("4", 4),
               ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)]
 
-println(mapreduce(x->parse(Int,"$(last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (findfirst(ns[1], x), ns[2]), numstrings))))))$(last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (findlast(ns[1], x), ns[2]), numstrings))))))"), +, input))
+println(mapreduce(x->parse(Int,mapreduce(f->map(x->"$x", last(reduce((x, y) -> x[1] < y[1] ? x : y, map(x->(first(x[1]), x[2]), filter(x->x[1] !== nothing, map(ns -> (f.(ns[1], x), ns[2]), numstrings)))))), *, [findfirst, findlast])), +, input))
