ea0b8d56245040232a9923b49bdaa4eea1159192
Carl Wiede <guardsmancarl@gmail.com>
2023-12-04 11:56:02 +0100

    A little prettier mby

--- day4/day4.jl
+++ day4/day4.jl
@@ -1,10 +1,7 @@
 include("../ReadString.jl")
 lines = ReadString.read_input()
 
-nr_scratchcards = ones(Int, length(lines))
-matches_in_cards = zeros(Int, length(lines))
-
-function parse_card(card)
+function get_matches_for_card(card)::Int
     numbers = split(card, ":")[2]
     winning_side = split(numbers, "|")[1]
     my_side = split(numbers, "|")[2]
@@ -20,17 +17,17 @@ function parse_card(card)
         n -> parse(Int, n),
         my_numbers
     )
-    return (winning_ints, my_ints)
-end
-
-function get_value_of_card(card)
-    winning_ints, my_ints = parse_card(card)
     matches = 0
     for num in my_ints
         if in(num, winning_ints)
             matches += 1
         end
     end
+    return matches
+end
+
+function get_value_of_card(card)
+    matches = get_matches_for_card(card)
     if matches > 0
         winning_value = 1 * 2^(matches-1)
         return winning_value
@@ -46,16 +43,8 @@ function part1(input::Vector{String})::Int
     )
 end
 
-function count_matches(card, i)
-    winning_ints, my_ints = parse_card(card)
-    matches = 0
-    for num in my_ints
-        if in(num, winning_ints)
-            matches += 1
-        end
-    end
-    matches_in_cards[i] = matches
-end
+nr_scratchcards = ones(Int, length(lines))
+matches_in_cards = zeros(Int, length(lines))
 
 function process_matches()
     for i in 1:203
@@ -68,7 +57,7 @@ end
 function part2(input::Vector{String})::Int
     for i in 1:203
         card = input[i]
-        count_matches(card, i)
+        matches_in_cards[i] = get_matches_for_card(card)
     end
     process_matches()
     return reduce(+, nr_scratchcards)
