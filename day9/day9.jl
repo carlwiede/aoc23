b249b89266bed287b7b49fa6c30153e4b22a534f
Carl Wiede <guardsmancarl@gmail.com>
2023-12-08 09:41:54 +0100

    Prep day 9

---
+++ day9/day9.jl
@@ -0,0 +1,13 @@
+include("../ReadString.jl")
+lines = ReadString.read_input()
+
+function part1(input::Vector{String})::Int
+    return 0
+end
+
+function part2(input::Vector{String})::Int
+    return 0
+end
+
+@show part1(lines)
+@show part2(lines)
