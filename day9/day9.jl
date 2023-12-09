d0720d06f94760e7d5f34037ed7fded7475011e4
Carl Wiede <guardsmancarl@gmail.com>
2023-12-09 16:21:19 +0100

    Much neater

--- day9/day9.jl
+++ day9/day9.jl
@@ -1,10 +1,8 @@
 include("../ReadFile.jl")
 lines = ReadFile.read_input()
 
-function value_of_line1(line)
+function get_graph(line)
     all_vals = [map(n -> parse(Int, n), split(line, " "))]
-
-    # Generate graph
     index = 1
     while !all(v -> v == 0, all_vals[index])
         new_vals = []
@@ -14,42 +12,28 @@ function value_of_line1(line)
         push!(all_vals, new_vals)
         index += 1
     end
-
-    # Get value
-    push!(last(all_vals), 0)
-    for i in length(all_vals)-1:-1:1
-        push!(all_vals[i], last(all_vals[i+1]) + last(all_vals[i]))
-    end
-
-    return last(all_vals[1])
+    all_vals
 end
 
-function value_of_line2(line)
-    all_vals = [map(n -> parse(Int, n), split(line, " "))]
-
-    # Generate graph
-    index = 1
-    while !all(v -> v == 0, all_vals[index])
-        new_vals = []
-        for i in 1:length(all_vals[index])-1
-            push!(new_vals, all_vals[index][i+1] - all_vals[index][i])
-        end
-        push!(all_vals, new_vals)
-        index += 1
+function value_of_line1(graph)
+    push!(last(graph), 0)
+    for i in length(graph)-1:-1:1
+        push!(graph[i], last(graph[i+1]) + last(graph[i]))
     end
+    last(graph[1])
+end
 
-    # Get value
-    prepend!(last(all_vals), 0)
-    for i in length(all_vals)-1:-1:1
-        prepend!(all_vals[i], first(all_vals[i]) - first(all_vals[i+1]))
+function value_of_line2(graph)
+    prepend!(last(graph), 0)
+    for i in length(graph)-1:-1:1
+        prepend!(graph[i], first(graph[i]) - first(graph[i+1]))
     end
-
-    return first(all_vals[1])
+    first(graph[1])
 end
 
 function part1(input::Vector{String})::Int
     mapreduce(
-        l -> value_of_line1(l),
+        l -> value_of_line1(get_graph(l)),
         +,
         input
     )
@@ -57,7 +41,7 @@ end
 
 function part2(input::Vector{String})::Int
     mapreduce(
-        l -> value_of_line2(l),
+        l -> value_of_line2(get_graph(l)),
         +,
         input
     )
