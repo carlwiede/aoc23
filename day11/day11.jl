881ca4e841212dede7c1ab711499744bd0253960
Carl Wiede <guardsmancarl@gmail.com>
2023-12-11 15:14:36 +0100

    Just some code golfing

--- day11/day11.jl
+++ day11/day11.jl
@@ -1,68 +1,13 @@
 include("../ReadFile.jl")
 lines = ReadFile.read_input()
 
-function part1(input::Vector{String})::Int
-    star_positions = []
+function solve(input, exp_mult)::Int
+
+    # Expansion multiplier
+    multi = exp_mult - 1
 
     # Get stars
-    for y in 1:length(input)
-        for x in 1:length(input[1])-1
-            if input[y][x] == '#'
-                push!(star_positions, (y, x))
-            end
-        end
-    end
-
-    new_input = []
-
-    for y in 1:length(input)
-        push!(new_input, input[y])
-        if isempty(filter(p -> p[1] == y, star_positions))
-            push!(new_input, input[y])
-        end
-    end
-
-    added = 0
-    for x in 1:length(input[1])-1
-        if isempty(filter(p -> p[2] == x, star_positions))
-            for y in 1:length(new_input)
-                new_input[y] = string(new_input[y][1:x+added], '.', new_input[y][x+1+added:end])
-            end
-            added += 1
-        end
-    end
-
-    input = copy(new_input)
-
-    # Get stars again
     star_positions = []
-    for y in 1:length(input)
-        for x in 1:length(input[1])-1
-            if input[y][x] == '#'
-                push!(star_positions, (y, x))
-            end
-        end
-    end
-
-    lengths = 0
-    for i in 1:length(star_positions)
-        for j in 1+i:length(star_positions)
-            dy = abs(star_positions[i][1] - star_positions[j][1])
-            dx = abs(star_positions[i][2] - star_positions[j][2])
-            lengths += dx + dy
-        end
-    end
-    
-    return lengths
-end
-
-function part2(input::Vector{String})::Int
-
-    multi = 1000000000 - 1
-
-    star_positions = []
-
-    # Get stars
     for y in 1:length(input)
         for x in 1:length(input[1])-1
             if input[y][x] == '#'
@@ -71,6 +16,7 @@ function part2(input::Vector{String})::Int
         end
     end
 
+    # Update expanded y positions
     for y in 1:length(input)
         if isempty(filter(p -> p[1] == y, star_positions))
             for (i, p) in enumerate(star_positions)
@@ -81,6 +27,7 @@ function part2(input::Vector{String})::Int
         end
     end
 
+    # Update expanded x positions
     for x in 1:length(input[1])-1
         if isempty(filter(p -> p[2] == x, star_positions))
             for (i, p) in enumerate(star_positions)
@@ -91,6 +38,7 @@ function part2(input::Vector{String})::Int
         end
     end
 
+    # Calculate steps
     lengths = 0
     for i in 1:length(star_positions)
         for j in 1+i:length(star_positions)
@@ -98,10 +46,9 @@ function part2(input::Vector{String})::Int
             dx = abs((star_positions[i][2] + star_positions[i][4]*multi) - (star_positions[j][2] + star_positions[j][4]*multi))
             lengths += dx + dy
         end
-    end
-    
+    end 
     return lengths
 end
 
-@show part1(lines)
-@show part2(lines)
+@show solve(lines, 2) # Part 1
+@show solve(lines, 1000000) # Part 2
