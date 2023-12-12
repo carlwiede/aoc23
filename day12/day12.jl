41d060bd7c05f894eee47a6f88fac644f2d0737f
Carl Wiede <guardsmancarl@gmail.com>
2023-12-12 13:49:26 +0100

    Very functional and not completely original solve

--- day12/day12.jl
+++ day12/day12.jl
@@ -1,13 +1,48 @@
 include("../ReadFile.jl")
 lines = ReadFile.read_input()
 
-function part1(input::Vector{String})::Int
-    return 0
+cache = Dict()
+
+function get_comb(s, nums)
+    cached_val = get(cache, (s, nums), -1)
+    cached_val != -1 && return cached_val
+
+    length(nums) == 0 && return all(c -> c != '#', s) ? 1 : 0
+
+    num = nums[1]
+    combs = 0
+
+    for i in eachindex(s)
+        if  i + num - 1 <= length(s) &&
+            all(c -> c != '.', s[i:i+num-1]) &&
+            (i + num - 1 == length(s) || s[i+num] != '#')
+
+            combs += get_comb(s[i+num+1:end], nums[2:end])
+        end
+
+        if s[i] == '#'
+            break
+        end
+    end
+
+    cache[(s, nums)] = combs
+
+    return combs
 end
 
-function part2(input::Vector{String})::Int
-    return 0
+function solve(input::Vector{String}, times)::Int
+    mapreduce(
+        line -> 
+        begin
+            springs, groups = split(line)
+            springs = join(repeat([springs], times), "?")
+            groups = repeat([parse(Int, n) for n in split(groups, ",")], times)
+            get_comb(springs, groups)
+        end,
+        +,
+        input
+    )
 end
 
-@show part1(lines)
-@show part2(lines)
+@show solve(lines, 1)
+@show solve(lines, 5)
