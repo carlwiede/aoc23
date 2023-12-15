a9e88e1619c5a1a675a98e2d7f0855cad0b6df1a
Carl Wiede <guardsmancarl@gmail.com>
2023-12-15 11:26:54 +0100

    Finally got 'proper' solve up and I'm not touching this with a very long stick ever again

---
+++ day13/day13.jl
@@ -0,0 +1,135 @@
+include("../ReadFile.jl")
+lines = ReadFile.read_input()
+
+function assert_mirror(grid, start, stop)::Int
+    potential_value = start
+    while true
+        start -= 1
+        stop += 1
+
+        if start < 1 || stop > length(grid)
+            return potential_value
+        end
+
+        if grid[start] != grid[stop]
+            return 0
+        end
+    end
+end
+
+function part1(input::Vector{String})::Int
+    total = 0
+
+    # Load grids
+    grids = []
+    current = []
+    for line in input
+        if line == "" || line == "\r"
+            push!(grids, current)
+            current = []
+        else
+            push!(current, line)
+        end
+    end
+
+    popindices = []
+
+    # Check vertically
+    for i in eachindex(grids)
+        vert_grid = [join(chars) for chars in zip(grids[i]...)][1:end-1]
+        for j in 1:length(vert_grid)-1
+            if vert_grid[j] == vert_grid[j+1]
+                val = assert_mirror(vert_grid, j, j+1)
+                if val > 0
+                    push!(popindices, i)
+                    total += val
+                    @goto end_outer_v
+                end
+            end
+        end
+        @label end_outer_v
+    end
+
+    splice!(grids, popindices)
+
+    # Check horizontally
+    for i in eachindex(grids)
+        for j in 1:length(grids[i])-1
+            if grids[i][j] == grids[i][j+1]
+                val = assert_mirror(grids[i], j, j+1)
+                if val > 0
+                    total += val * 100
+                    @goto end_outer_h
+                end
+            end
+        end
+        @label end_outer_h
+    end
+    return total
+end
+
+using Distances
+
+function assert_smudge(grid, start, stop)
+    potential_value = start
+    smudges = 0
+    while true
+        start -= 1
+        stop += 1
+
+        if start < 1 || stop > length(grid)
+            return (smudges, potential_value)
+        end
+
+        smudges += hamming(grid[start], grid[stop])
+    end
+
+end
+
+function part2(input::Vector{String})::Int
+    total = 0
+
+    # Load grids
+    grids = []
+    current = []
+    for line in input
+        if line == "" || line == "\r"
+            push!(grids, current)
+            current = []
+        else
+            push!(current, line)
+        end
+    end
+
+    for i in eachindex(grids)
+        vert_grid = [join(chars) for chars in zip(grids[i]...)][1:end-1]
+        for j in length(vert_grid)-1:-1:1
+            diff = hamming(vert_grid[j], vert_grid[j+1])
+            if diff in [0,1]
+                (smudges, val) = assert_smudge(vert_grid, j, j+1)
+                if diff + smudges == 1
+                    total += val
+                    @goto end_v
+                end
+            end
+        end
+        @label end_v
+    end
+    for i in eachindex(grids)
+        for j in length(grids[i])-1:-1:1
+            diff = hamming(grids[i][j], grids[i][j+1])
+            if diff in [0,1]
+                (smudges, val) = assert_smudge(grids[i], j, j+1)
+                if diff + smudges == 1
+                    total += val * 100
+                    @goto end_h
+                end
+            end
+        end
+        @label end_h
+    end
+    return total
+end
+
+@show part1(lines)
+@show part2(lines)
