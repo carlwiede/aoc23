8e4646bbef1f98650b627494cf8795d3832b4a98
Carl Wiede <guardsmancarl@gmail.com>
2023-12-15 09:19:23 +0100

    Prep day 16

---
+++ day16/day16.jl
@@ -0,0 +1,13 @@
+include("../ReadFile.jl")
+lines = ReadFile.read_input()
+
+function part1(input::Vector{String})::Int
+    return 0
+end
+
+function part2(input::Vector{String})::Int
+    return 0
+end
+
+@show part1(lines)
+@show part2(lines)
