452777bbc34a5793e65ec4a78559e6e84a732055
Carl Wiede <guardsmancarl@gmail.com>
2023-12-19 14:23:24 +0100

    Prep day 20

---
+++ day20/day20.jl
@@ -0,0 +1,12 @@
+lines = readlines("input.txt")
+
+function part1(input::Vector{String})::Int
+    return 0
+end
+
+function part2(input::Vector{String})::Int
+    return 0
+end
+
+@show part1(lines)
+@show part2(lines)
